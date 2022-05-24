951af164017cdcba6183a050b7d41d9db9c4b269
pb2640 <pb2640@nyu.edu>
2022-05-24 14:29:03 -0400

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # make a hashmap by iterating over the list
+        hashmap = {}
+        for i in range(len(nums)):
+            if(nums[i] not in hashmap):
+                hashmap[nums[i]]  = [i]
+            else:
+                hashmap[nums[i]].append(i)
+        
+        for i in range(len(nums)):
+            if(target-nums[i] in hashmap):
+                if(len(hashmap[target-nums[i]])>1):
+                    for it in hashmap[target-nums[i]]:
+                        if it!= i:
+                            return[i,it]
+                elif(hashmap[target-nums[i]][0]!=i):
+                    
+                   return [i,hashmap[target-nums[i]][0]] 
