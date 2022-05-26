3a093a027e7ebd007ba7b956b0b269f0e58dc333
pb2640 <pb2640@nyu.edu>
2022-05-26 00:51:59 -0400

    Create # 389 find the difference.py

---
+++ hashmap/# 389 find the difference.py
@@ -0,0 +1,11 @@
+# 389 find the difference
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        counter_s = Counter(s)
+        
+        for char in t:
+            if char not in counter_s or counter_s[char]==0:
+                return char
+            else:
+                counter_s[char]-=1
