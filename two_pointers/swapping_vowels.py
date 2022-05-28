6364c9cf764607fc91b8dcfd7827bcbd0c20b305
pb2640 <pb2640@nyu.edu>
2022-05-28 12:45:24 -0400

    Update swapping_vowels.py

--- two_pointers/swapping_vowels.py
+++ two_pointers/swapping_vowels.py
@@ -1,20 +1,19 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        ref = set(['a','e','i','o','u','A','E','I','O','U'])
+        ref = set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
         l = 0
-        r = len(s)-1
+        r = len(s) - 1
         ans = list(s)
-        while(l<r):
-            if(s[l] in ref and s[r] in ref):
+        while l < r:
+            if s[l] in ref and s[r] in ref:
                 temp = ans[l]
                 ans[l] = ans[r]
                 ans[r] = temp
-                l+=1
-                r-=1
+                l += 1
+                r -= 1
                 continue
-            if(s[l] not in ref):
-                l+=1
-            if(s[r] not in ref):
-                r-=1
+            if s[l] not in ref:
+                l += 1
+            if s[r] not in ref:
+                r -= 1
         return "".join(ans)
-        
