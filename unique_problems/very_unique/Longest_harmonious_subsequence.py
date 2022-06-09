b85fed331a383f81a55d5417b2a3cf1e87819785
pb2640 <pb2640@nyu.edu>
2022-06-09 07:40:54 -0400

    Update Longest_harmonious_subsequence.py

--- unique_problems/very_unique/Longest_harmonious_subsequence.py
+++ unique_problems/very_unique/Longest_harmonious_subsequence.py
@@ -1,16 +1,37 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        ans = 0
         hashmap = {}
+        ans = 0
+        unique = set()
+        nums.sort()
         for i in range(len(nums)):
-            if(nums[i] in hashmap):
-                continue
+            if nums[i] in hashmap:
+                hashmap[nums[i]][1] = i
             else:
-                hashmap[nums[i]] = 1
-            for j in range(i+1,len(nums)-1):
-                if(nums[j]==nums[i]-1):
-                    hashmap[nums[i]]+=1
-            if(ans<hashmap[nums[i]]):
-                ans = hashmap[nums[i]]
+                hashmap[nums[i]] = [i, i]
+            unique.add(nums[i])
+        # print(hashmap)
+        # calcaulate max
+        for num in unique:
+            if num + 1 in hashmap:
+                ans = max(ans, (hashmap[num + 1][1] + 1 - hashmap[num][0]))
         return ans
-                
+
+        # brute force approach
+
+
+#         ans = 0
+
+#         for i in range(len(nums)):
+#             count = 0
+#             flag = False
+#             for j in range(len(nums)):
+#                 if(nums[j]==nums[i]):
+#                     count+=1
+#                 elif(nums[j]==nums[i]+1):
+#                     count+=1
+#                     flag = True
+#             if(flag):
+#                 ans = max(ans,count)
+
+#         return ans
