f3995a07a93a28cdcf147e23122ee880a66a3269
pb2640 <pb2640@nyu.edu>
2022-06-12 15:58:52 -0400

    Update min_stack_constant_time.py

--- unique_problems/very_unique/min_stack_constant_time.py
+++ unique_problems/very_unique/min_stack_constant_time.py
@@ -1,20 +1,15 @@
 class MinStack:
-
     def __init__(self):
         self.arr = []
-        
 
     def push(self, val: int) -> None:
         self.arr.append(val)
-        
 
     def pop(self) -> None:
         return self.arr.pop(-1)
-        
 
     def top(self) -> int:
         return self.arr[-1]
-        
 
     def getMin(self) -> int:
         return sorted(self.arr)[0]
@@ -29,29 +24,24 @@ class MinStack:
 
 ###IMPROVED VERSION#######
 class MinStack:
-
     def __init__(self):
         self.arr = []
         # self.mini = float("inf")
-        
 
     def push(self, val: int) -> None:
-        if(len(self.arr)==0):
-            self.arr.append([val,val])
-        elif(val<self.arr[-1][1]):
-            self.arr.append([val,val])
+        if len(self.arr) == 0:
+            self.arr.append([val, val])
+        elif val < self.arr[-1][1]:
+            self.arr.append([val, val])
         else:
-            self.arr.append([val,self.arr[-1][1]])
-        
+            self.arr.append([val, self.arr[-1][1]])
 
     def pop(self) -> None:
         self.arr.pop(-1)
         # return item[0]
-        
 
     def top(self) -> int:
         return self.arr[-1][0]
-        
 
     def getMin(self) -> int:
         return self.arr[-1][1]
