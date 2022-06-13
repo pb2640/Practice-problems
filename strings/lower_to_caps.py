237c2dee8f96eddff08d8fb5ec966bdcb087e0b2
pb2640 <pb2640@nyu.edu>
2022-06-13 10:58:42 -0400

    Solved lower to upper using other strat

--- strings/lower_to_caps.py
+++ strings/lower_to_caps.py
@@ -1,13 +1,23 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        big = "QWERTYUIOPASDFGHJKLZXCVBNM"
-        small = "qwertyuiopasdfghjklzxcvbnm"
-        big = set(big)
-        small = set(small)
+        # for every letter check if it is upper
+        is_upper  = lambda x: 'A'<=x<='Z'
         ans = ""
         for letter in s:
-            if letter in big:
-                ans += letter.lower()
+            if(is_upper(letter)):
+                ans+=chr(ord(letter)+32)
             else:
-                ans += letter
+                ans+=letter
         return ans
+        # big = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        # small = "qwertyuiopasdfghjklzxcvbnm"
+        # big  =  set(big)
+        # small = set(small)
+        # ans = ""
+        # for letter in s:
+        #     if letter in big:
+        #         ans+=letter.lower()
+        #     else:
+        #         ans+=letter
+        # return ans
+        
