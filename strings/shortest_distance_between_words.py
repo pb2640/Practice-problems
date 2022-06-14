235381d86740913b23df0c0e7e4afc4b6650a110
pb2640 <pb2640@nyu.edu>
2022-06-14 11:39:09 -0400

    Update shortest_distance_between_words.py

--- strings/shortest_distance_between_words.py
+++ strings/shortest_distance_between_words.py
@@ -1,16 +1,34 @@
+# class Solution:
+#     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+#         w1 = [] 
+#         w2 = []
+#         for i in range(len(wordsDict)):
+#             if(word1==wordsDict[i]):
+#                 w1.append(i)
+#             elif(word2==wordsDict[i]):
+#                 w2.append(i)
+        
+#         mini = float("inf")
+#         for i in w1:
+#             for j in w2:
+#                 mini = min(mini,abs(i-j))
+                
+#         return mini
+
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        w1 = [] 
-        w2 = []
+        i1,i2 = -1,-1
+        mini = len(wordsDict)
         for i in range(len(wordsDict)):
             if(word1==wordsDict[i]):
-                w1.append(i)
+                i1 = i
             elif(word2==wordsDict[i]):
-                w2.append(i)
-        
-        mini = float("inf")
-        for i in w1:
-            for j in w2:
-                mini = min(mini,abs(i-j))
-                
+                i2 = i
+            if(i1!=-1 and i2!=-1):
+                mini = min(mini,abs(i1-i2))
+            if(mini==1):
+                return 1
         return mini
+                
+        
+        
