7007c35fb1ce3e5528c87535ec440f7c128afe7b
pb2640 <pb2640@nyu.edu>
2022-06-14 11:39:36 -0400

    alignment

--- strings/Excel_sheet.py
+++ strings/Excel_sheet.py
@@ -3,9 +3,8 @@ class Solution:
         ref = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         dic = {}
         for i in range(len(ref)):
-            dic[ref[i]] = i+1
+            dic[ref[i]] = i + 1
         ans = 0
         for i in range(len(columnTitle)):
-            ans+=dic[columnTitle[len(columnTitle)-i-1]]*(26**i)
+            ans += dic[columnTitle[len(columnTitle) - i - 1]] * (26 ** i)
         return ans
-            
