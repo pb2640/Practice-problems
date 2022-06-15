f3986a3b5436883f96382166ba140435e0d7f80c
pb2640 <pb2640@nyu.edu>
2022-06-15 12:02:17 -0400

    Create add_binary.py

---
+++ strings/add_binary.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a)-1
+        j  =len(b)-1
+        carry = 0
+        ans =""
+        while(i>=0 or j>=0):
+            if(i<0):
+                temp = int(b[j])+carry
+                carry = temp//2
+                dig = temp%2
+            elif(j<0):
+                temp = int(a[i])+carry
+                carry = temp//2
+                dig = temp%2
+            else:
+                temp = int(a[i])+int(b[j])+carry
+                carry = temp//2
+                dig = temp%2
+            ans = str(dig)+ans    
+            i-=1
+            j-=1
+        if(carry):
+            ans = str(carry)+ans
+        return ans
+                
