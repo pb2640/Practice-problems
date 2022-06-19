138d20c0fe8457b48b6b14422d07b8a3ecb4bd66
pb2640 <pb2640@nyu.edu>
2022-06-19 15:34:43 -0400

    Update finding_peak.py

--- search/finding_peak.py
+++ search/finding_peak.py
@@ -1,19 +1,16 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        l,r = 0,len(arr)-1
-        while(l<=r):
-            mid = (l+r)//2
-            if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1]):
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
                 return mid
-            elif(arr[mid]>arr[mid+1]):
-                r = mid-1
+            elif arr[mid] > arr[mid + 1]:
+                r = mid - 1
             else:
-                l = mid+1
-                
-        
-        #approach # O(n)
+                l = mid + 1
+
+        # approach # O(n)
         # for i in range(len(arr)-2):
         #     if(arr[i+1]>arr[i] and arr[i+1]>arr[i+2]):
         #         return i+1
-            
-        
