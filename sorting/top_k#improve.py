e6e6934183367a4edfd375999e3a8125dacb2cb4
pb2640 <pb2640@nyu.edu>
2022-06-30 11:50:54 -0400

    Update top_k#improve.py

--- sorting/top_k#improve.py
+++ sorting/top_k#improve.py
@@ -1,20 +1,28 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         ref = {}
+#         for num in nums:
+#             if num in ref:
+#                 ref[num] += 1
+#             else:
+#                 ref[num] = 1
+#         arr = []
+#         for num in ref:
+#             arr.append([num, ref[num]])
+#         arr.sort(key=lambda x: -x[1])
+#         # print(arr)
+#         ans = []
+#         count = 0
+#         while count < k:
+#             ans.append(arr[count][0])
+#             count += 1
+
+#         return ans
+
+from collections import Counter
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ref = {}
-        for num in nums:
-            if num in ref:
-                ref[num] += 1
-            else:
-                ref[num] = 1
-        arr = []
-        for num in ref:
-            arr.append([num, ref[num]])
-        arr.sort(key=lambda x: -x[1])
-        # print(arr)
-        ans = []
-        count = 0
-        while count < k:
-            ans.append(arr[count][0])
-            count += 1
-
-        return ans
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
