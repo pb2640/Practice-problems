6f48ff1ee960b8cbd78f68a7f483c0389168f395
pb2640 <pb2640@nyu.edu>
2022-06-30 09:33:48 -0400

    Create top_k#improve.py

---
+++ sorting/top_k#improve.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ref = {}
+        for num in nums:
+            if num in ref:
+                ref[num] += 1
+            else:
+                ref[num] = 1
+        arr = []
+        for num in ref:
+            arr.append([num, ref[num]])
+        arr.sort(key=lambda x: -x[1])
+        # print(arr)
+        ans = []
+        count = 0
+        while count < k:
+            ans.append(arr[count][0])
+            count += 1
+
+        return ans
