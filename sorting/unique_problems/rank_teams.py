f5474343bf34926f0bcd7182f4b725742d97ed6e
pb2640 <pb2640@nyu.edu>
2022-07-04 19:42:21 -0400

    Create rank_teams.py

---
+++ sorting/unique_problems/rank_teams.py
@@ -0,0 +1,26 @@
+1366. Rank Teams by Votes
+Input: votes = ["ABC","ACB","ABC","ACB","ACB"]
+Output: "ACB"
+'''
+Idea, for ever vote in votes. Add the team in a hashmap and record no. of votes it got for each position
+then sort the poistions alphabetically and then by the votes and join to get the final string.
+'''
+
+
+
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        ref = {}
+        for vote in votes:
+            for i in range(len(vote)):
+                if(vote[i] in ref):
+                    ref[vote[i]][i]+=1
+                else:
+                    ref[vote[i]]= [0]*len(vote)
+                    ref[vote[i]][i] = 1
+        
+        ans = sorted(ref.keys())
+     
+        return "".join(sorted(ans,key = lambda x:ref[x],reverse = True))
+
+        
