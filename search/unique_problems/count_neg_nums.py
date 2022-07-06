ffb51b98f3d085187904ea4f0b67d035c7c712ea
pb2640 <pb2640@nyu.edu>
2022-07-06 13:24:37 -0400

    Update count_neg_nums.py

--- search/unique_problems/count_neg_nums.py
+++ search/unique_problems/count_neg_nums.py
@@ -1,38 +1,37 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        '''
-    strat 2: find the first neg element using binary search and count remaining neg using index
-    O(mlogn)
-    '''
+        """
+        strat 2: find the first neg element using binary search and count remaining neg using index
+        O(mlogn)
+        """
         length = len(grid[0])
         count = 0
         for i in range(len(grid)):
             l = 0
-            r = length-1
+            r = length - 1
             row = grid[i]
-            while(l<=r):
-                
-                mid = (l+r)//2
+            while l <= r:
+
+                mid = (l + r) // 2
                 # print(mid,row[mid],row[mid-1])
-                if(mid==0 and row[mid]<0):
-                    count+=length
+                if mid == 0 and row[mid] < 0:
+                    count += length
                     break
-                elif(row[mid]<0 and row[mid-1]>=0):
-                    count+=(length-mid)
+                elif row[mid] < 0 and row[mid - 1] >= 0:
+                    count += length - mid
                     break
-                elif(row[mid]<0):
-                    r = mid-1
+                elif row[mid] < 0:
+                    r = mid - 1
                 else:
-                    l = mid+1
-                    
+                    l = mid + 1
+
             # print(i,count)
-                
-        return count 
-        
-        
-        '''
+
+        return count
+
+        """
         strat 1: iterate over each element in each row, on first neg number break and calc remaining using index
-        '''
+        """
         # length = len(grid[0])
         # count = 0
         # for i in range(len(grid)):
@@ -41,4 +40,3 @@ class Solution:
         #             count+=(length-j)
         #             break
         # return count
-    
