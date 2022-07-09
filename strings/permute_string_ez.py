ef26f922499bfdb672ab357f65a5dbbe771eab2c
pb2640 <pb2640@nyu.edu>
2022-07-08 20:11:18 -0400

    Update permute_string_ez.py

--- strings/permute_string_ez.py
+++ strings/permute_string_ez.py
@@ -3,15 +3,14 @@ class Solution:
         ref = {}
         for letter in s:
             if letter in ref:
-                ref[letter]+=1
+                ref[letter] += 1
             else:
                 ref[letter] = 1
         ans = ""
         for letter in order:
             if letter in ref:
-                ans+=letter*ref[letter]
+                ans += letter * ref[letter]
                 ref.pop(letter)
         for letter in ref:
-            ans+=letter*ref[letter]
+            ans += letter * ref[letter]
         return ans
-        
