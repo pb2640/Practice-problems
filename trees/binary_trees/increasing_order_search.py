01a57d7e4c51ba70893e54d7d215a31062e4d6d9
pb2640 <pb2640@nyu.edu>
2022-07-09 14:42:37 -0400

    Update increasing_order_search.py

--- trees/binary_trees/increasing_order_search.py
+++ trees/binary_trees/increasing_order_search.py
@@ -4,29 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
+"""
 Do an inorder traversal and keep making nodes and adding them to the previous node
-'''
+"""
+
+
 class Solution:
     def __init__(self):
         self.head = TreeNode()
         self.node = self.head
+
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        '''
+        """
         if left and right node are absent, add the node to the linked list
         if a left node exists, recurse on that node
         add root node to the list
         if right node exist recurse on right node
-        '''
-        if(not root.left and not root.right):
+        """
+        if not root.left and not root.right:
             self.node.right = TreeNode(root.val)
             self.node = self.node.right
             return root
-        if(root.left):
+        if root.left:
             self.increasingBST(root.left)
         self.node.right = TreeNode(root.val)
         self.node = self.node.right
-        if(root.right):
+        if root.right:
             self.increasingBST(root.right)
         return self.head.right
-        
