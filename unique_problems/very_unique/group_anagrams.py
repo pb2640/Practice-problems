a01117c41c4a1c4b580d280ada14cb84c35ea114
pb2640 <pb2640@nyu.edu>
2022-07-12 20:31:53 -0400

    Update group_anagrams.py

--- unique_problems/very_unique/group_anagrams.py
+++ unique_problems/very_unique/group_anagrams.py
@@ -1,29 +1,27 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
+        """
         take w1, w2
-        build a hashmap from w1, take letter l in w2, if letter in hashmap and by the end of iteration all keys are zero 
+        build a hashmap from w1, take letter l in w2, if letter in hashmap and by the end of iteration all keys are zero
         add w2 to the temp array and remove w2 from array and w1 by then end of iterating strs
         do the same till you iterate over all words in strs
-        
+
         Problem with above code: O(n^2k)
-        
+
         try:
-        sort the word -> returns list, convert to tuple now this tuple as a key and append the word 
+        sort the word -> returns list, convert to tuple now this tuple as a key and append the word
         if the sorted tuple is present in hashmap
         return values of this hashmap
-        '''
+        """
         ans = {}
         for word in strs:
-            #sort the word
+            # sort the word
             sword = tuple(sorted(word))
             if sword in ans:
                 ans[sword].append(word)
             else:
                 ans[sword] = [word]
-        
-        
-            
+
         return ans.values()
         # ans = []
         # for i in range(len(strs)):
