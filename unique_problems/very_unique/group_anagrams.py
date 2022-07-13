07826b9be0d7b4572c976608b65307c46785fdd4
pb2640 <pb2640@nyu.edu>
2022-07-12 20:21:38 -0400

    Create group_anagrams.py

---
+++ unique_problems/very_unique/group_anagrams.py
@@ -0,0 +1,52 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        take w1, w2
+        build a hashmap from w1, take letter l in w2, if letter in hashmap and by the end of iteration all keys are zero
+        add w2 to the temp array and remove w2 from array and w1 by then end of iterating strs
+        do the same till you iterate over all words in strs
+        """
+        ans = []
+        for i in range(len(strs)):
+            w1 = strs[i]
+            # if word is marked, it has been compared with other words so skip it
+            if w1 == 1:
+                continue
+            # create a hashmap to check if letters present in w1 are present in w2
+            hashmap_orig = {}
+            for letter in w1:
+                if letter in hashmap_orig:
+                    hashmap_orig[letter] += 1
+                else:
+                    hashmap_orig[letter] = 1
+            arr = []
+            flag = 0
+            # iterate over rest of the words
+            for j in range(i + 1, len(strs)):
+                hashmap = hashmap_orig.copy()
+                w2 = strs[j]
+                if w2 == 1:
+                    continue
+                for letter in w2:
+                    if letter in hashmap:
+                        hashmap[letter] -= 1
+                    else:
+                        # compare with other word
+                        flag = 1
+                        break
+                if flag:
+                    flag = 0
+                    continue
+                for letter in hashmap:
+                    if hashmap[letter] != 0:
+                        flag = 1
+                        break
+                if flag:
+                    flag = 0
+                    continue
+                arr.append(strs[j])
+                strs[j] = 1
+            arr.append(strs[i])
+            ans.append(arr)
+
+        return ans
