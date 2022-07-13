c21ff787c00f3a9a1b8f991ef91714375af3b302
pb2640 <pb2640@nyu.edu>
2022-07-13 13:40:42 -0400

    Update two_sum.py

--- hashmap/two_sum.py
+++ hashmap/two_sum.py
@@ -3,20 +3,8 @@ class Solution:
         # make a hashmap by iterating over the list
         hashmap = {}
         for i in range(len(nums)):
-            if nums[i] not in hashmap:
-                hashmap[nums[i]] = [i]
+            if target - (nums[i]) in hashmap:
+                hashmap[target - nums[i]].append(i)
+                return hashmap[target - nums[i]]
             else:
-                hashmap[nums[i]].append(i)
-
-        for i in range(len(nums)):
-            if target - nums[i] in hashmap:
-                if len(hashmap[target - nums[i]]) > 1:
-                    for it in hashmap[target - nums[i]]:
-                        if it != i:
-                            return [i, it]
-                elif hashmap[target - nums[i]][0] != i:
-
-                    return [i, hashmap[target - nums[i]][0]]
-
-
-####################### IDEAL SOLUTION ###############################
+                hashmap[nums[i]] = [i]
