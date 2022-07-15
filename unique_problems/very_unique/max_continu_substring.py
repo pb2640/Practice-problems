d51720d000221b8b25474477920bb26d2a66d463
pb2640 <pb2640@nyu.edu>
2022-07-15 19:28:56 -0400

    Update max_continu_substring.py

--- unique_problems/very_unique/max_continu_substring.py
+++ unique_problems/very_unique/max_continu_substring.py
@@ -1,23 +1,38 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
         i = 0
-        max_len = 0
-        while i < len(s):
-            temp_hash = set()
-            j = i
-            temp_str = ""
-            mp = {}
-            next_idx = 0
-            while j < len(s):
-                if s[j] not in temp_hash:
-                    temp_str += s[j]
-                    temp_hash.add(s[j])
-                    mp[s[j]] = j
-                    j += 1
-                else:
-                    next_idx = mp[s[j]]
-                    break
-            if len(temp_str) > max_len:
-                max_len = len(temp_str)
-            i += 1
-        return max_len
+        hashmap = {}
+        for j in range(len(s)):
+            if s[j] in hashmap:
+                i = max(i, hashmap[s[j]] + 1)
+
+            ans = max(ans, j - i + 1)
+            # print(ans)
+            hashmap[s[j]] = j
+        return ans
+
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         i = 0
+#         max_len = 0
+#         while i < len(s):
+#             temp_hash = set()
+#             j = i
+#             temp_str = ""
+#             mp = {}
+#             next_idx = 0
+#             while j < len(s):
+#                 if s[j] not in temp_hash:
+#                     temp_str += s[j]
+#                     temp_hash.add(s[j])
+#                     mp[s[j]] = j
+#                     j += 1
+#                 else:
+#                     next_idx = mp[s[j]]
+#                     break
+#             if len(temp_str) > max_len:
+#                 max_len = len(temp_str)
+#             i += 1
+#         return max_len
