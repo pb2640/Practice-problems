06dfd6425311a858d28e23c87385b1f4abe41e83
pb2640 <pb2640@nyu.edu>
2022-07-16 00:39:42 -0400

    Create valid_anagram.py

---
+++ hashmap/valid_anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = {}
+        for letter in s:
+            if letter in hashmap:
+                hashmap[letter]+=1
+            else:
+                hashmap[letter] = 1
+        for letter in t:
+            if letter not in hashmap:
+                return False
+            else:
+                hashmap[letter]-=1
+        for item in hashmap:
+            if(hashmap[item]!=0):
+                return False
+        return True
+        
