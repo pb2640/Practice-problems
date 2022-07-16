ab34e1aa7adc867f48824290d73a5af29d70b4ed
pb2640 <pb2640@nyu.edu>
2022-07-16 00:39:48 -0400

    Update valid_anagram.py

--- hashmap/valid_anagram.py
+++ hashmap/valid_anagram.py
@@ -3,16 +3,15 @@ class Solution:
         hashmap = {}
         for letter in s:
             if letter in hashmap:
-                hashmap[letter]+=1
+                hashmap[letter] += 1
             else:
                 hashmap[letter] = 1
         for letter in t:
             if letter not in hashmap:
                 return False
             else:
-                hashmap[letter]-=1
+                hashmap[letter] -= 1
         for item in hashmap:
-            if(hashmap[item]!=0):
+            if hashmap[item] != 0:
                 return False
         return True
-        
