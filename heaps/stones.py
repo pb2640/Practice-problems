511c92e58f90ff1c3565ce3ad8853f4820236474
pb2640 <pb2640@nyu.edu>
2022-07-21 12:17:23 -0400

    Update stones.py

--- heaps/stones.py
+++ heaps/stones.py
@@ -1,21 +1,19 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        if(len(stones)==1):
+        if len(stones) == 1:
             return stones[0]
         heap = []
         for val in stones:
-            heapq.heappush(heap,-1*val)
-        
-        
-        
-        while(len(heap)>1):
+            heapq.heappush(heap, -1 * val)
+
+        while len(heap) > 1:
             # print("initial",heap)
-            ele1 = heapq.heappop(heap)*-1
-            ele2 = heapq.heappop(heap)*-1
+            ele1 = heapq.heappop(heap) * -1
+            ele2 = heapq.heappop(heap) * -1
             # print(ele1,ele2)
-            if(ele1==ele2):
-                heapq.heappush(heap,0)
+            if ele1 == ele2:
+                heapq.heappush(heap, 0)
             else:
-                heapq.heappush(heap,abs(ele1-ele2)*-1)
+                heapq.heappush(heap, abs(ele1 - ele2) * -1)
             # print("after",heap)
-        return heap[0]*-1
+        return heap[0] * -1
