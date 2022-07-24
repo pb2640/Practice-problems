19dff1d17d9a93af12de1e36e0a9facaa20b2b5b
pb2640 <pb2640@nyu.edu>
2022-07-24 14:04:24 -0400

    Create max_subarray.py

---
+++ unique_problems/very_unique/max_subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if(len(nums)==1):
+            return nums[0]
+        ans = nums[0]
+        temp_max = nums[0]
+        for i in range(1,len(nums)):
+            
+            temp_max=max(nums[i],nums[i]+temp_max)
+            
+            ans = max(ans,temp_max)
+                
+                
+        return ans
+        
