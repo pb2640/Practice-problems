e2c81f78d721dafbf217b71708245a52e8d45f84
pb2640 <pb2640@nyu.edu>
2022-07-28 14:51:41 -0400

    Update add+two_nums.py

--- linked_lists/add+two_nums.py
+++ linked_lists/add+two_nums.py
@@ -4,7 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         # def reverse(head):
         #     prev = None
         #     curr = head
@@ -17,25 +19,22 @@ class Solution:
         def calc(head):
             i = 0
             num = 0
-            while(head):
-                num+=head.val*(10**i)
-                i+=1
+            while head:
+                num += head.val * (10 ** i)
+                i += 1
                 head = head.next
             return num
-                
+
         l1 = calc(l1)
         l2 = calc(l2)
-        num = l1+l2
-        if(num==0):
+        num = l1 + l2
+        if num == 0:
             return ListNode(0)
         ref = ListNode(-1)
         node = ref
-        while(num!=0):
-            add = ListNode(num%10)
-            num = num//10
+        while num != 0:
+            add = ListNode(num % 10)
+            num = num // 10
             node.next = add
             node = add
         return ref.next
-            
-            
-        
