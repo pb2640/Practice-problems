88025aa425cb9fb39dd4f8ce9c524716bf90d57f
pb2640 <pb2640@nyu.edu>
2022-07-31 15:33:22 -0400

    Update climbing_stairs.py

--- dp/1d/climbing_stairs.py
+++ dp/1d/climbing_stairs.py
@@ -3,15 +3,14 @@ class Solution:
         self.hashmap = {}
         self.hashmap[1] = 1
         self.hashmap[2] = 2
-        
+
     def climbStairs(self, n: int) -> int:
-        if(n==1):
+        if n == 1:
             return 1
-        if(n==2):
+        if n == 2:
             return 2
-        if(n in self.hashmap):
+        if n in self.hashmap:
             return self.hashmap[n]
         else:
-            self.hashmap[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
-            return(self.climbStairs(n-1)+self.climbStairs(n-2))
-        
+            self.hashmap[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
