fd327092e5e1b94c96828b1d9ec90f5508a29ad3
pb2640 <pb2640@nyu.edu>
2022-08-05 16:46:25 -0400

    Update max_water.py

--- two_pointers/max_water.py
+++ two_pointers/max_water.py
@@ -2,13 +2,13 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         a = -float("inf")
         pt1 = 0
-        pt2 = len(height)-1
-        while(pt1<pt2):
-            l = min(height[pt1],height[pt2])
-            b = pt2-pt1
-            a = max(a,l*b)
-            if(height[pt1]<=height[pt2]):
-                pt1+=1
+        pt2 = len(height) - 1
+        while pt1 < pt2:
+            l = min(height[pt1], height[pt2])
+            b = pt2 - pt1
+            a = max(a, l * b)
+            if height[pt1] <= height[pt2]:
+                pt1 += 1
             else:
-                pt2-=1
+                pt2 -= 1
         return a
