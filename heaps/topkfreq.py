9601358da997962737cb7f52065e832bf4b7f49a
pb2640 <pb2640@nyu.edu>
2022-08-06 18:24:13 -0400

    Create topkfreq.py

---
+++ heaps/topkfreq.py
@@ -0,0 +1,40 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        record = {}
+        for num in nums:
+            if num in record:
+                record[num]+=1
+            else:
+                record[num] = 1
+                
+        arr = []
+        for key in record:
+            arr.append([key,record[key]])
+        arr.sort(key = lambda x:-x[1])
+        ans = []
+        for i in range(k):
+            ans.append(arr[i][0])
+        return ans
+        
+'''
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        record = {}
+        for num in nums:
+            if num in record:
+                record[num]+=1
+            else:
+                record[num] = 1
+                
+        arr = []
+        for key in record:
+            arr.append((-record[key],key))
+        heap = arr
+        heapq.heapify(heap)
+        ans = []                      
+        for i in range(k):
+            ans.append(heapq.heappop(heap)[1])
+            heapq.heapify(heap)
+        return ans
+'''
