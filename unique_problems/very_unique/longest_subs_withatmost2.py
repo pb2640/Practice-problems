a777e723ba1c0c6378cbe7041267a776eee98d2d
pb2640 <pb2640@nyu.edu>
2022-08-12 20:02:42 -0400

    Update longest_subs_withatmost2.py

--- unique_problems/very_unique/longest_subs_withatmost2.py
+++ unique_problems/very_unique/longest_subs_withatmost2.py
@@ -4,14 +4,13 @@ class Solution:
         for i in range(len(s)):
             seen = set()
             j = i
-            
-            while(j<len(s)):
-                if(s[j] not in seen):
+
+            while j < len(s):
+                if s[j] not in seen:
                     seen.add(s[j])
-                if(len(seen)>2):
+                if len(seen) > 2:
                     break
-                j+=1
-                
-            ans = max(ans,j-i)
+                j += 1
+
+            ans = max(ans, j - i)
         return ans
-                    
