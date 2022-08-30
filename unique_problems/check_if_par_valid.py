f3a3a439b2f624760de108d0d6bddba4feac41e8
pb2640 <pb2640@nyu.edu>
2022-08-30 16:56:28 -0400

    Create check_if_par_valid.py

---
+++ unique_problems/check_if_par_valid.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if(len(s)%2!=0):
+            return False
+        bal = 0
+        for i in range(len(s)):
+            if(locked[i]=="0" or s[i]=="("):
+                bal+=1
+            else:
+                bal-=1
+            if(bal<0):
+                return False
+        bal = 0
+        for i in range(len(s)-1,-1,-1):
+            if(locked[i]=="0" or s[i]==")"):
+                bal+=1
+            else:
+                bal-=1
+            if(bal<0):
+                return False
+        return True
