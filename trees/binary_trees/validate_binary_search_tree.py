23624a19426ce4f66baf57e4fa0cbf64c7e89f0b
pb2640 <pb2640@nyu.edu>
2022-08-31 10:41:25 -0400

    Update validate_binary_search_tree.py

--- trees/binary_trees/validate_binary_search_tree.py
+++ trees/binary_trees/validate_binary_search_tree.py
@@ -5,29 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(
-        self, root: Optional[TreeNode], low=-float("inf"), high=float("inf")
-    ) -> bool:
-        # print(high)
-        if not root.left and not root.right and root.val < high and root.val > low:
-            return True
-        if root.left and root.right:
-            if (
-                root.val > root.left.val
-                and root.val < root.right.val
-                and root.val < high
-                and root.val > low
-            ):
-                return self.isValidBST(
-                    root.left, low=low, high=min(high, root.val)
-                ) and self.isValidBST(root.right, low=max(root.val, low), high=high)
-            else:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def bst(node,low = -math.inf,high = math.inf):
+            if(node==None):
+                return True
+            if(node.val<=low or node.val>=high):
                 return False
-        if root.left and not root.right:
-            if root.val > root.left.val:
-                return self.isValidBST(root.left, low=low, high=min(high, root.val))
-            else:
-                return False
-        if root.right:
-            if root.val < root.right.val:
-                return self.isValidBST(root.right, low=max(root.val, low), high=high)
+            return bst(node.left,low,node.val) and bst(node.right,node.val,high)
+        return bst(root)
+        
