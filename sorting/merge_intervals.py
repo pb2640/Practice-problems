f49d3e0def55a78d66ec35edcf952d2319534688
pb2640 <pb2640@nyu.edu>
2022-09-12 16:11:52 -0400

    Create merge_intervals.py

---
+++ sorting/merge_intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:x[0])
+        
+        ans = [intervals[0]]
+        for i in range(1,len(intervals)):
+            interval = intervals[i]
+            #if there is an intersection, update end
+            if(interval[0]>=ans[-1][0] and interval[0]<=ans[-1][1]):
+                ans[-1][1] = max(ans[-1][1],interval[1])
+            else:
+                ans.append(interval)
+        return ans
+        
+        
