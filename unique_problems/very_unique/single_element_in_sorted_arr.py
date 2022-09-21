30900efc84a24096b2f7866117cf45f359b3409e
pb2640 <pb2640@nyu.edu>
2022-09-21 18:42:34 -0400

    Update single_element_in_sorted_arr.py

--- unique_problems/very_unique/single_element_in_sorted_arr.py
+++ unique_problems/very_unique/single_element_in_sorted_arr.py
@@ -1,23 +1,22 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
-        if(n==1):
+        if n == 1:
             return nums[0]
         l = 0
-        r = n-1
-        while(l<=r):
-            mid = (l+r)//2
+        r = n - 1
+        while l <= r:
+            mid = (l + r) // 2
             # print(mid)
-            if(mid-1>=0 and nums[mid-1]==nums[mid]):
-                if(mid%2==0):
-                    r = mid-2
+            if mid - 1 >= 0 and nums[mid - 1] == nums[mid]:
+                if mid % 2 == 0:
+                    r = mid - 2
                 else:
-                    l = mid+1
-            elif(mid+1<=n-1 and nums[mid+1]==nums[mid]):
-                if((mid+1)%2==0):
-                    r = mid-1
+                    l = mid + 1
+            elif mid + 1 <= n - 1 and nums[mid + 1] == nums[mid]:
+                if (mid + 1) % 2 == 0:
+                    r = mid - 1
                 else:
-                    l = mid+2
+                    l = mid + 2
             else:
                 return nums[mid]
-            
