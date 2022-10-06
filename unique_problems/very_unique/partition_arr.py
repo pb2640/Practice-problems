85d56f6aaa627e2402d1b4090e8a76316b9cac45
pb2640 <pb2640@nyu.edu>
2022-10-06 00:28:34 -0400

    Create partition_arr.py

---
+++ unique_problems/very_unique/partition_arr.py
@@ -0,0 +1,14 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        res = 1
+        nums.sort()
+        mx,mn = nums[0],nums[0]
+        for num in nums:
+            mn = min(num,mn)
+            mx = max(num,mx)
+            if(mx-mn>k):
+                res+=1
+                mx=mn=num
+        return res
+            
+            
