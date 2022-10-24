ae9289b1e6f752d30d0d3cbba9cb9205df76f52c
pb2640 <pb2640@nyu.edu>
2022-10-23 20:08:24 -0400

    Create make_array_zero.py

---
+++ unique_problems/very_unique/make_array_zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        ans = set()
+        for x in nums:
+            if(x>0):
+                ans.add(x)
+                
+        return len(ans)
+       
