b7d5d4d16699d487154a6fd19c88609225850e85
pb2640 <pb2640@nyu.edu>
2022-10-25 16:57:25 -0400

    Update inert_intervals.py

--- sorting/inert_intervals.py
+++ sorting/inert_intervals.py
@@ -2,27 +2,32 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
-        ans = []
         i = 0
-
+        flag_add_to_end = 1
+        ans = []
         new_start, new_end = newInterval[0], newInterval[1]
-        # add all intervals before new interval
-        while i < len(intervals) and new_start > intervals[i][0]:
-            ans.append(intervals[i])
-            i += 1
-        # add new Interval
-        # if no overlap add the interval
-        if not ans or ans[-1][1] < new_start:
-            ans.append(newInterval)
-
-        # if there is an overlap
-        else:
-            ans[-1][1] = max(ans[-1][1], new_end)
-
         while i < len(intervals):
-            if ans[-1][1] >= intervals[i][0]:
-                ans[-1][1] = max(ans[-1][1], intervals[i][1])
-            else:
+            curr_start, curr_end = intervals[i][0], intervals[i][1]
+            if curr_end < new_start:
                 ans.append(intervals[i])
+            else:
+                flag_add_to_end = 0
+                if new_end < curr_start:
+                    ans.append(newInterval)
+
+                else:
+                    ans.append([min(curr_start, new_start), max(curr_end, new_end)])
+                break
+            i += 1
+        if flag_add_to_end:
+            ans.append(newInterval)
+            return ans
+        while i < len(intervals):
+            curr_start, curr_end = intervals[i][0], intervals[i][1]
+            if ans[-1][1] < intervals[i][0]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+
             i += 1
         return ans
