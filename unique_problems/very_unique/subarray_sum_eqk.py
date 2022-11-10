7429df5497c787748cf7269fd676ec32dfc9713f
pb2640 <pb2640@nyu.edu>
2022-11-10 15:35:54 -0500

    Update subarray_sum_eqk.py

--- unique_problems/very_unique/subarray_sum_eqk.py
+++ unique_problems/very_unique/subarray_sum_eqk.py
@@ -1,31 +1,45 @@
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+
+#         f = 0
+#         s = 0
+#         n = len(nums)
+#         temp_sum = nums[0]
+#         ans = 0
+#         while f < n and s < n:
+#             if temp_sum == k:
+#                 ans += 1
+#                 if f <= s:
+#                     s += 1
+#                     if s < n:
+#                         temp_sum += nums[s]
+#             elif temp_sum < k:
+#                 s += 1
+#                 if s < n:
+#                     temp_sum += nums[s]
+#             else:
+#                 if f < s:
+#                     temp_sum -= nums[f]
+#                     f += 1
+
+#                 else:
+#                     temp_sum -= nums[f]
+#                     f += 1
+#                     s += 1
+#                     if f < n:
+#                         temp_sum += nums[f]
+#         return ans
+
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        sums = [0] * (len(nums) + 1)
 
-        f = 0
-        s = 0
-        n = len(nums)
-        temp_sum = nums[0]
+        for i in range(1, len(nums) + 1):
+            sums[i] = sums[i - 1] + nums[i - 1]
         ans = 0
-        while f < n and s < n:
-            if temp_sum == k:
-                ans += 1
-                if f <= s:
-                    s += 1
-                    if s < n:
-                        temp_sum += nums[s]
-            elif temp_sum < k:
-                s += 1
-                if s < n:
-                    temp_sum += nums[s]
-            else:
-                if f < s:
-                    temp_sum -= nums[f]
-                    f += 1
-
-                else:
-                    temp_sum -= nums[f]
-                    f += 1
-                    s += 1
-                    if f < n:
-                        temp_sum += nums[f]
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if sums[j + 1] - sums[i] == k:
+                    ans += 1
         return ans
